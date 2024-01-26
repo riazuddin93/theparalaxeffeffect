464e23fe6696803b0badb03c7df939391dfa01fc
riazuddin93 <riazuddin@protonmail.com>
2024-01-26 21:44:08 +0000

    title added

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 let sofa = document.getElementById('sofa');
 let chair1 = document.getElementById('chair1');
 let chair2 = document.getElementById('chair2');
+let title = document.getElementById('title')
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     sofa.style.left = value * -0.5 + 'px';
     chair2.style.left = value * 1.3 + 'px';
     chair1.style.left = value * -1.5 + 'px';
+    title.style.marginTop = value * 2.5 + 'px';
 });
