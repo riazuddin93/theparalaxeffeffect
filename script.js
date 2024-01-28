bdf49fa04be7f3be1e59ca7094e048a1f899171d
riazuddin93 <riazuddin@protonmail.com>
2024-01-28 17:59:33 +0000

    navbar added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let sofa = document.getElementById('sofa');
 let chair1 = document.getElementById('chair1');
 let chair2 = document.getElementById('chair2');
 let title = document.getElementById('title')
+let nav = document.getElementById('nav');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -10,4 +11,6 @@ window.addEventListener('scroll', () => {
     chair2.style.left = value * 1.3 + 'px';
     chair1.style.left = value * -1.5 + 'px';
     title.style.marginTop = value * 2.5 + 'px';
+    nav.style.opacity = value * 1 + '%';
+    nav.style.marginTop = value * 1 + 'px';
 });
